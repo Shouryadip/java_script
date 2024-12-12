b7b903e43b8a80ce8bfaf68ab8c72e37313485ab
Shouryadip <143379528+Shouryadip@users.noreply.github.com>
2024-12-12 14:09:04 +0000

    files

---
+++ 01_basic/1_variable.js
@@ -0,0 +1,16 @@
+const accountId=12245
+let accountEmail="shourya1@gmail.com"
+var accountPassword="1225"
+accountCity="Kolkata"
+let accountstate;
+// accountId=2  //not allowed
+
+accountEmail="abc4@gmailcom"
+accountPassword="5958"
+accountCity="Burdwan"
+/*
+prefer not to use var
+bz problem in block or scope
+*/
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity,accountstate])
