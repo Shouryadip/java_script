ed93988ab93941a773e9ba045db6bc48a3dbee14
Shouryadip <143379528+Shouryadip@users.noreply.github.com>
2024-12-12 14:22:06 +0000

    dataTypes

---
+++ 01_basic/2_datatypes.js
@@ -0,0 +1,16 @@
+let Name="Shourya"
+let age=19
+let isLoggedIn=false
+let state;
+
+// number= 2 to the power 53
+//string= ""
+// undefined
+// boolean== true//false
+// symbol==unique
+
+//object
+
+console.log(typeof age)
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
