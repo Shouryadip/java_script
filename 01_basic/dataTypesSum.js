bedb187a16bc33292c4d573fe622bbcdf37cc867
Shouryadip <143379528+Shouryadip@users.noreply.github.com>
2024-12-13 15:34:46 +0000

    dataTypes Summary

---
+++ 01_basic/dataTypesSum.js
@@ -0,0 +1,27 @@
+/*Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+*/
+
+
+
+
+
+
+/* Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+*/
+
+let myFunction = function(){
+    console.log("Hello World");
+}
+
+const hero=["shourya","array","example"];
+console.log(hero)
