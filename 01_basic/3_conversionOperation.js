10067d057642dea7f28b08167c79874d88fbaf88
Shouryadip <143379528+Shouryadip@users.noreply.github.com>
2024-12-13 15:13:29 +0000

    conversion

---
+++ 01_basic/3_conversionOperation.js
@@ -0,0 +1,14 @@
+//number to string conversion
+let someNumber=33
+let StringNumber=String(someNumber)
+console.log(typeof someNumber)
+console.log(typeof StringNumber)
+console.log(StringNumber)
+
+//String to number conversion
+
+let Sno="33"
+let No=Number(Sno)
+console.log(typeof Sno)
+console.log(typeof No)
+console.log(No)
