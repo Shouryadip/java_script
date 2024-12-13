7ec225b32037ffb934842be51a1a6afe15088849
Shouryadip <143379528+Shouryadip@users.noreply.github.com>
2024-12-13 15:49:17 +0000

    datatypes and memory

--- 01_basic/dataTypesSum.js
+++ 01_basic/dataTypesSum.js
@@ -25,3 +25,26 @@ let myFunction = function(){
 
 const hero=["shourya","array","example"];
 console.log(hero)
+
+
+//memory
+
+// stack(premitive)     heap(non-premetive)
+
+//stack
+let name="Shourya"
+let anothername=name
+anothername="Ranti"
+console.log(name);
+console.log(anothername);
+
+//heap
+let user1={
+    Name:"Shourya",
+    age:20
+}
+let user2=user1
+user2.Name="Ranti"
+console.log(user1.Name);
+console.log(user2.Name);
+
